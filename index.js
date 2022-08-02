b654e3dd9b93440631f50ff6083d3aabe2c632f9
facundou <facundo@lightit.io>
2022-08-02 09:55:00 -0300

    Json y post complete

--- index.js
+++ index.js
@@ -1,84 +1,68 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const fs = require('fs');
-
+const cors = require("cors");
+const fs = require("fs");
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/contactos', (req, res) => {
-    fs.readFile('./contactos.json', (err, data) => {
-        if (err) throw err;
-        res.send(JSON.parse(data));
-    }
-    );
-}
-);
+app.get("/contactos", (req, res) => {
+  fs.readFile("./contactos.json", (err, data) => {
+    if (err) throw err;
+    res.send(JSON.parse(data));
+  });
+});
 
-app.get('/contactos/:id', (req, res) => {
-    fs.readFile('./contactos.json', (err, data) => {
-        if (err) throw err;
-        res.send(JSON.parse(data));
-    }
-    );
-}
-);
+app.get("/contactos/:id", (req, res) => {
+  fs.readFile("./contactos.json", (err, data) => {
+    if (err) throw err;
+    let contactos = JSON.parse(data);
+    let contacto = contactos.find((contacto) => contacto.id == req.params.id);
+    res.send(contacto);
+  });
+});
 
-// app.post('/contactos', (req, res) => {
-//     fs.readFile('./contactos.json', (err, data) => {    
-//         if (err) throw err;
-//         let contactos = JSON.parse(data);
-//         contactos.push(req.body);
-//         fs.writeFile('./contactos.json', JSON.stringify(contactos), (err) => {
-//             if (err) throw err;
-//             res.send(contactos);
-//         }
-//         );
-//     }
-//     );
-// }
-// );
+app.post("/contactos", (req, res) => {
+  fs.readFile("./contactos.json", (err, data) => {
+    if (err) throw err;
+    let contactos = JSON.parse(data);
+    contactos.push(req.body);
+    fs.writeFile("./contactos.json", JSON.stringify(contactos), (err) => {
+      if (err) throw err;
+      res.send(contactos);
+    });
+  });
+});
 
-app.delete('/contactos/:id', (req, res) => {
-    fs.readFile('./contactos.json', (err, data) => {
-        if (err) throw err;
-        let contactos = JSON.parse(data);
-        contactos = contactos.filter(contacto => contacto.id != req.params.id);
-        fs.writeFile('./contactos.json', JSON.stringify(contactos), (err) => {
-            if (err) throw err;
-            res.send(contactos);
-        }
-        );
-    }
-    );
-}
-);
+app.delete("/contactos/:id", (req, res) => {
+  fs.readFile("./contactos.json", (err, data) => {
+    if (err) throw err;
+    let contactos = JSON.parse(data);
+    contactos = contactos.filter((contacto) => contacto.id != req.params.id);
+    fs.writeFile("./contactos.json", JSON.stringify(contactos), (err) => {
+      if (err) throw err;
+      res.send(contactos);
+    });
+  });
+});
 
-app.put('/contactos/:id', (req, res) => {
-    fs.readFile('./contactos.json', (err, data) => {
-        if (err) throw err;
-        let contactos = JSON.parse(data);
-        contactos = contactos.map(contacto => {
-            if (contacto.id == req.params.id) {
-                contacto = req.body;
-            }
-            return contacto;
-        }
-        );
-        fs.writeFile('./contactos.json', JSON.stringify(contactos), (err) => {
-            if (err) throw err;
-            res.send(contactos);
-        }
-        );
-    }
-    );
-}
-);
-
-//loooooooooooooooooool//
+app.put("/contactos/:id", (req, res) => {
+  fs.readFile("./contactos.json", (err, data) => {
+    if (err) throw err;
+    let contactos = JSON.parse(data);
+    contactos = contactos.map((contacto) => {
+      if (contacto.id == req.params.id) {
+        contacto = req.body;
+      }
+      return contacto;
+    });
+    fs.writeFile("./contactos.json", JSON.stringify(contactos), (err) => {
+      if (err) throw err;
+      res.send(contactos);
+    });
+  });
+});
 
 app.listen(3000, () => {
-    console.log('Server running on port 3000');
-}
-);
+  console.log("Server running on port 3000");
+});
